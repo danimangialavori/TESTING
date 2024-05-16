139ec3b9902f9b97c6143d98811cd830bad11546
danimangialavori <danielmangialavori@gmail.com>
2024-05-15 21:23:34 -0300

    form 15-5

---
+++ FORMULARIO CLASE/Scriptsform.js
@@ -0,0 +1,16 @@
+
+function validacion()
+{
+    var  nombre = document.getElementById("nombre").value;
+    var  apellido = document.getElementById("apellido").value;
+    var  calle = document.getElementById("calle").value;
+    var  numero = document.getElementById("numero").value;
+    var  ciudad = document.getElementById("ciudad").value;
+    var  codearea = document.getElementById("codearea").value;
+    var  numtelefono = document.getElementById("numtelefono").value;
+    var email =document.getElementById("email").value;
+    var  edad = document.getElementById("edad").value;
+    var fechadenacimiento =document.getElementById("fechadenacimiento").value
+    var  genero = document.getElementById("genero").value;
+    
+    }
---
+++ FORMULARIO CLASE/funcionesform.js
@@ -0,0 +1,58 @@
+$(document).ready(function() 
+{
+
+insertar_registro();
+
+
+}
+)
+function insertar_registro()
+{
+$(document).on('click', '#btn_register', function() 
+
+{
+var nombre = $('#nombre').val();
+var apellido= $('#apellido').val();
+var calle= $('#calle').val();
+var numero= $('#numero').val();
+var ciudad= $('#ciudad').val();
+var codearea= $('#codearea').val();
+var numtelefono= $('#numtelefono').val();
+var email= $('#email').val();
+var edad= $('#edad').val();
+var fechadenacimiento= $('#fechadenacimiento').val();
+var genero= $('#genero').val();
+
+if(nombre == "" || apellido == "" || calle == ""   || numero == ""  || ciudad == "" ||  email == ""  || edad == "" || fechadenacimiento == "" || genero == "" ) 
+{
+    $('#Mensaje').html ("Llenar los campos en blanco");
+}
+else
+{
+
+    $.ajax(
+        {
+            URL: 'insertar.php',
+            method: 'post',
+            data: {Unombre:nombre, Uapellido:apellido, Ucalle:calle, Unumero:numero, Uciudad:ciudad, Ucodearea:codearea, Unumtelefono:numtelefono, UEmail:email, Uedad:edad, Ufechadenacimiento:fechadenacimiento, Ugenero:genero},
+            success: function (data)
+        {
+            //$('#Mensaje').html (data);
+           //$('#Mensaje').html ('Se Inserto Bien');
+
+           // $('#Registration').modal ('show');
+           // $('form').trigger ('reset')
+           // Mostrar_registro();
+
+          // alert("Entro a la funcion");
+          // alert(data);
+
+        } 
+
+
+
+        }
+    )
+}
+})
+}
